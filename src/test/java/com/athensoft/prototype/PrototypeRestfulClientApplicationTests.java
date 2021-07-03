0ebff36f9513105bff863b7c0c86ff67ecf6cc30
YiyuanShang <yiyuan.shang@mail.mcgill.ca>
2021-07-03 15:41:29 -0400

    initial commit

---
+++ src/test/java/com/athensoft/prototype/PrototypeRestfulClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athensoft.prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeRestfulClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
